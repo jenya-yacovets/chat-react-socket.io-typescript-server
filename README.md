8dbc88df3b9b69a66783745f44f2244dd1dede22
Jenya Yacovets <jenya.yacovets@gmail.com>
2021-05-02 16:16:16 +0300

    Initial server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chat-react-socket.io-nodejs
 A chat that works with React, Socket.io, Node Js
+
+NodeJs version: 16.0.0
---
+++ server/socket.js
@@ -0,0 +1,8 @@
+export default io => {
+   
+    io.on('connection', (socket) => {
+
+        console.log('User connection to socket')
+        
+    })
+}
